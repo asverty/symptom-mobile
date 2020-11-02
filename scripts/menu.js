0fa07a97774b2093bf70745fcdf997bd5cea2e7d
Tim <virtualxself@gmail.com>
2020-11-02 20:16:15 +0200

    Сверстал главную страницу без футера

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,8 @@
 const menuPopup = document.querySelector('.menu');
 const toggleMenu = () => menuPopup.classList.toggle('menu_hidden');
 
-const menuButton = document.querySelector('.header__menu-button');
+// .header-main - для главной страницы, .header - для остальных
+const menuButton = document.querySelector('.header-main__menu-button') || document.querySelector('.header__menu-button');
 menuButton.addEventListener('click', toggleMenu);
 
 const closeMenuButton = document.querySelector('.menu__close-button');
