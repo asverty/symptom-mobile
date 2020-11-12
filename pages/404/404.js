681dee16dcaaf9d10c9be766686cb4ea3f1d0c88
Tim <virtualxself@gmail.com>
2020-11-12 18:04:45 +0200

    Переверстал страницы брендов и категорий

--- pages/404/404.js
+++ pages/404/404.js
@@ -1,15 +1,15 @@
 // МЕНЮ
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.menu-popup');
 const header = document.querySelector('.header');
-const error404 = document.querySelector('.error-404');
-const menuButton = document.querySelector('.header__menu-button');
-const closeMenuButton = document.querySelector('.menu__close-button');
+const main = document.querySelector('.main');
+const menuButton = document.querySelector('.menu-button');
+const closeButton = document.querySelector('.menu-close-button');
 
 const toggleMenu = () => {
-  menu.classList.toggle('menu_hidden');
-  error404.classList.toggle('error-404_hidden');
+  menu.classList.toggle('menu-popup_hidden');
   header.classList.toggle('header_hidden');
+  main.classList.toggle('main_hidden');
 }
 
 menuButton.addEventListener('click', toggleMenu);
-closeMenuButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
